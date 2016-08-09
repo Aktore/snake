931be9bdaae314d9a9ac854ad48a6f9b0e30f924
Aktore <aktore.niyazymbetov@gmail.com>
2016-08-09 23:35:59 +0600

    создал класс для горизонтальной и вертикальной линий

---
+++ snake/snake/VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class VerticalLine // класс который будет рисовать горизотальную линию
+    {
+        List<Point> pList;
+        public VerticalLine( int yTop, int ybottom, int x, char sym)
+        {
+            pList = new List<Point>();
+            for(int y = yTop; y <= ybottom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void yDraw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
